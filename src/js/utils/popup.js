c13f8ae091d30760512b91e6d44fcc1716a151ba
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-02-01 16:27:34 +0400

    add contacts page

--- src/js/utils/popup.js
+++ src/js/utils/popup.js
@@ -1,45 +1,9 @@
 // body lock
 import { modules } from '../dev/kuloverova';
-// import { bodyLockStatus, bodyUnlock, bodyLock } from "../utils/constants";
+import { bodyLockStatus, bodyUnlock, bodyLock } from "../utils/constants";
 
 const popup = () => {
-  let bodyLockStatus = true;
-  let bodyUnlock = (delay = 500) => {
-    let body = document.querySelector('body');
-    if (bodyLockStatus) {
-      let lock_padding = document.querySelectorAll('[data-lp]');
-      setTimeout(() => {
-        for (let index = 0; index < lock_padding.length; index++) {
-          const el = lock_padding[index];
-          el.style.paddingRight = '0px';
-        }
-        body.style.paddingRight = '0px';
-        document.documentElement.classList.remove('lock');
-      }, delay);
-      bodyLockStatus = false;
-      setTimeout(function () {
-        bodyLockStatus = true;
-      }, delay);
-    }
-  };
-  let bodyLock = (delay = 500) => {
-    let body = document.querySelector('body');
-    if (bodyLockStatus) {
-      let lock_padding = document.querySelectorAll('[data-lp]');
-      for (let index = 0; index < lock_padding.length; index++) {
-        const el = lock_padding[index];
-        el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-      }
-      body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-      document.documentElement.classList.add('lock');
-
-      bodyLockStatus = false;
-      setTimeout(function () {
-        bodyLockStatus = true;
-      }, delay);
-    }
-  };
-
+ 
   // launch ======================================================================
   class Popup {
     constructor(options) {
